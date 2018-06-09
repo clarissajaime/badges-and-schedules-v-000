f7c0bb52353337b83acae09d2fac60a589f47f61
Clarissa Jaime <clarissanjaime@gmail.com>
2018-06-09 20:52:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,7 @@
 
 names = ["Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(names)
+  names.each do |name|
     puts "Hello, my name is #{name}."
+  end
 end
-
-# names.each do |name|
-#   puts "Hello, my name is #{name}."
-# end
